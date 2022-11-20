694db422de67d2e4fc9b3b8cf1847857842b94cc
Seki Ryoma <85884346+rymsk-21@users.noreply.github.com>
2022-11-20 14:10:47 +0900

    apiを作成 (#1)

---
+++ app/javascript/application.js
@@ -0,0 +1,16 @@
+import "@hotwired/turbo-rails"
+import "./controllers"
+import React, { StrictMode } from "react";
+import {createRoot} from "react-dom/client";
+import App from "./components/App";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+document.addEventListener("DOMContentLoaded", () => {
+  root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+  );
+});
