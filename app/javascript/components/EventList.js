f4fe74f359bd955c23bc6992491476715ca8bf3d
Seki Ryoma <85884346+rymsk-21@users.noreply.github.com>
2022-11-20 20:36:01 +0900

    検索機能追加 (#3)

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState,useRef} from 'react';
 import PropTypes from 'prop-types';
 import { Link, NavLink } from 'react-router-dom';
 
 const EventList = ({ events }) => {
-  const renderEvents = (eventArray) => {
-    eventArray.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchInput = useRef(null);
 
-    return eventArray.map((event) => (
+  const updateSearchTerm = () => {
+    setSearchTerm(searchInput.current.value);
+  };
+
+  const matchSearchTerm = (obj) => {
+    const { id, published, created_at, updated_at, ...rest } = obj;
+    return Object.values(rest).some(
+      (value) => value.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1
+    );
+  };
+  
+  const renderEvents = (eventArray) =>
+  eventArray
+    .filter((el) => matchSearchTerm(el))
+    .sort((a, b) => new Date(b.event_date) - new Date(a.event_date))
+    .map((event) => (
       <li key={event.id}>
         <NavLink to={`/events/${event.id}`}>
           {event.event_date}
@@ -15,14 +30,21 @@ const EventList = ({ events }) => {
         </NavLink>
       </li>
     ));
-  };
 
   return (
     <section className='eventList'>
       <h2>
         Events
         <Link to="/events/new">New Event</Link>
-        </h2>
+      </h2>
+
+      <input
+        className='search'
+        placeholder='Search'
+        type='text'
+        ref={searchInput}
+        onKeyUp={updateSearchTerm}
+      />
       <ul>{renderEvents(events)}</ul>
     </section>
   );
